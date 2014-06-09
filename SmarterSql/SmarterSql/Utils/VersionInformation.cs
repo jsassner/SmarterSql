2740a2db70a74c74a813832f7b06d68cf7b95b05
johsas_cp <SND\johsas_cp>
2014-06-09 08:18:26 +0000

    Changed version nb

--- SmarterSql/SmarterSql/Utils/VersionInformation.cs
+++ SmarterSql/SmarterSql/Utils/VersionInformation.cs
@@ -6,6 +6,6 @@ using System;
 namespace Sassner.SmarterSql.Utils {
 	public static class VersionInformation {
 		public static int cintVersion = 1;
-		public static DateTime dtBuild = new DateTime(2012, 3, 15, 13, 11, 0);
+		public static DateTime dtBuild = new DateTime(2014, 6, 9, 10, 10, 0);
 	}
 }
